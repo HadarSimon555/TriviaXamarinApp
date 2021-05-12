26f9337338e13ee6043e29c898c4561878f8bf36
HadarSimon555 <77894680+HadarSimon555@users.noreply.github.com>
2021-05-12 11:39:28 +0300

    Add project files.

---
+++ TriviaXamarinApp/TriviaXamarinApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TriviaXamarinApp.ViewModels;
+namespace TriviaXamarinApp.ViewModels
+{
+    class LoginViewModel : ModelViewBase
+    {
+       
+    }
+}
