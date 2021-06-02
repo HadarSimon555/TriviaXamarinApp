d8ccb4564720aeca9f3bf53cdacbf466fd8d474d
User <User@LAB2-12>
2021-06-02 12:46:43 +0300

    Register

--- TriviaXamarinApp/TriviaXamarinApp/ViewModels/LoginViewModel.cs
+++ TriviaXamarinApp/TriviaXamarinApp/ViewModels/LoginViewModel.cs
@@ -6,6 +6,11 @@ namespace TriviaXamarinApp.ViewModels
 {
     class LoginViewModel : ModelViewBase
     {
-       
+        private string username, password;
+
+        public string UserName { get { return this.username; } set { if (this.username != value) { this.username = value; OnPropertyChange("UserName"); } } }
+        public string Password { get { return this.password; } set { if (this.password != value) { this.password = value; OnPropertyChange("Password"); } } }
+
+
     }
 }
